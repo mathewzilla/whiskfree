ad3221ffaccc180594d8575b84f86ea6ed947fe4
mathewzilla <mathewhevans@gmail.com>
2017-03-03 11:07:35 +0000

    retracking curation started

--- matlab/threepos_retracked.m
+++ matlab/threepos_retracked.m
@@ -11,7 +11,7 @@ retrack_db = {};
 dates =  [3,7,32,35,38,41,45];
 for i = 1:numel (dates)
     cd /mnt/NAS1/Dario/Behavioral_movies/32
-        files = dir;
+    files = dir;
     cd(files(dates(i)).name);
     files(dates(i)).name
     x = cellstr(ls);
@@ -31,7 +31,7 @@ end
 dates = [4,5,6,19];
 for i = 1:numel (dates)
     cd /mnt/NAS1/Dario/Behavioral_movies/33
-        files = dir;
+    files = dir;
     cd(files(dates(i)).name);
     files(dates(i)).name
     x = cellstr(ls);
@@ -52,7 +52,7 @@ end
 dates = [27,30,34,25,31,35,54,56,22,26];
 for i = 1:numel (dates)
     cd /mnt/NAS1/Dario/Behavioral_movies/34
-        files = dir;
+    files = dir;
     cd(files(dates(i)).name);
     files(dates(i)).name
     x = cellstr(ls);
@@ -70,10 +70,10 @@ for i = 1:numel (dates)
 end
 %% 36
 
-dates = [60,64,67,68,12,14,17,21,24]; 
+dates = [60,64,67,68,12,14,17,21,24];
 for i = 1:numel (dates)
-cd /mnt/isilon/fls/Dario' Campagner'/BEHAVIORAL' MOVIES'/36
-        files = dir;
+    cd /mnt/isilon/fls/Dario' Campagner'/BEHAVIORAL' MOVIES'/36
+    files = dir;
     cd(files(dates(i)).name);
     files(dates(i)).name
     x = cellstr(ls);
@@ -96,7 +96,7 @@ clear behav_38
 dates = [58,6,8,12,46,56,67,10,13,21]; % Touch detected sessions
 
 for i = 1:numel (dates)
-cd /mnt/isilon/fls/Dario' Campagner'/BEHAVIORAL' MOVIES'/38
+    cd /mnt/isilon/fls/Dario' Campagner'/BEHAVIORAL' MOVIES'/38
     files = dir;
     cd(files(dates(i)).name);
     files(dates(i)).name
@@ -116,9 +116,83 @@ end
 
 %% Save
 save ~/Dropbox/Data/3posdata/retrack_db retrack_db
+
+% Load spreadsheet.
+% Count ones in column L.
+
+%% Load retrack_db and count trials per mouse
+
+load ~/Dropbox/Data/3posdata/retrack_db.mat
+numtracked = zeros(5,1);
+for i = 1:size(retrack_db,1)
+   for j = 1:10;
+       x = retrack_db{i,j};
+       if numel(x)>=1
+           numtracked(i) = numtracked(i) + numel(x.tracked_files);
+       end
+   end
     
-   % Load spreadsheet.
-   % Count ones in column L.
-   
-   
-   
+end
+
+%% Work out which trials are AB, and count remainder
+load ~/Dropbox/Data/3posdata/retrack_db.mat
+% Re-tracked sessions: 
+% 32: 1:3,5:8. 33: 1:3,6. 34: 1:2,4:11. 36: 1:7,9,10. 38: 1:10;
+tracked_sess = zeros(5,13);
+% tracked_sess(1,[1:3,5:8]) = 1; 
+% tracked_sess(2,[1:3,6]) = 1; 
+% tracked_sess(3,[1,2,4:11]) = 1; 
+% Dropping bad sessions
+tracked_sess(1,[1:3,5:7]) = 1;
+tracked_sess(2,[2:3,6]) = 1;
+tracked_sess(3,[1,2,5:11]) = 1;
+tracked_sess(4,[1:7,9,10]) = 1;
+tracked_sess(5,1:10) = 1;
+
+% Strip out bad retrack_db sessions
+retrack_db{1,7} = [];
+retrack_db{2,1} = retrack_db{2,2};
+retrack_db{2,2} = retrack_db{2,3};
+retrack_db{2,3} = retrack_db{2,4};
+retrack_db{2,4} = [];
+for j = 3:9; retrack_db{3,j} = retrack_db{3,j+1}; end
+retrack_db{3,10} = [];
+
+
+a = [32,33,34,36,38];
+
+tracked_all = zeros(5,1);
+tracked_AB = zeros(5,1);
+
+for i = 1:5;
+load(['~/Dropbox/data/3posdata/meta_',num2str(a(i)),'.mat']);
+load(['~/Dropbox/data/3posdata/behav_',num2str(a(i)),'t.mat']);
+this_mouse = eval(['behav_',num2str(a(i))]);
+% this_touch = touch_38;
+this_meta = eval(['meta_',num2str(a(i))]);
+
+t_id = find(tracked_sess(i,:));
+
+    for j = 1:numel(t_id)
+        % Conditional for sessions without policy in
+        if (i==2 && t_id(j) == 6) || (i==3 && t_id(j) <6)
+            policy = ones(size(this_meta{t_id(j)}.hit));
+        else
+            policy = strcmp(this_meta{t_id(j)}.policy,'AB')';
+        end
+
+    
+    
+    tracked = retrack_db{i,j}.tracked_files;
+%     tt_m = this_meta{t_id(j)}.trialtype(find(policy));
+%     tracked = this_mouse{i}.tracked;
+%     tt_b = this_mouse{i}.trialtype;
+    tracked_all(i) = tracked_all(i) + numel(tracked);
+    tracked_AB(i) = tracked_AB(i) + sum(ismember(tracked,find(policy)));
+    end
+end
+tracked_all
+tracked_AB
+
+
+
